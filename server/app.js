c68094edbc5e49aaa4c9fa8cc2212046827c5951
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-03-13 13:53:59 +0400

    cors

--- server/app.js
+++ server/app.js
@@ -1,51 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const history = require('connect-history-api-fallback');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const cors_options = require("./config/cors_options");
+const credentials = require("./middleware/credentials");
+const history = require("connect-history-api-fallback");
+require("dotenv").config();
 
-
-const admin = require('./admin_api/routes/router.js');
-const client = require('./client_api/router.js');
-const auth = require('./admin_api/routes/authRoutes.js');
-
+const admin = require("./admin_api/routes/router.js");
+const client = require("./client_api/router.js");
+const auth = require("./admin_api/routes/authRoutes.js");
 
 const app = express();
 
 /*middleware*/
-app.use(express.static('api/public'));
+app.use(express.static("api/public"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+app.use(credentials);
 app.use(fileUpload());
 app.use(cookieParser());
-app.use(cors());
-app.use(history({
-    index: '/index.html'
-}));
+app.use(cors(cors_options));
+app.use(
+  history({
+    index: "/index.html",
+  })
+);
 
-
-
-const PORT =  process.env.PORT;
+const PORT = process.env.PORT;
 //const DB_URL = 'mongodb+srv://bootcamp:hy65Cb89P@bootcamp.i4bcb.mongodb.net/bootcamp?retryWrites=true&w=majority';
-const DB_URL="mongodb+srv://sokrat:lalala12345@cluster0.x2cvw.mongodb.net/catan?retryWrites=true&w=majority";
-async function start(){
-	try{
-		mongoose.connect(DB_URL, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			autoIndex: true,
-		})
-		app.listen(PORT, () => { console.log('Server started on port ' + PORT)});
-	}catch(e){
-		console.log(e);
-	}
+const DB_URL =
+  "mongodb+srv://sokrat:lalala12345@cluster0.x2cvw.mongodb.net/catan?retryWrites=true&w=majority";
+async function start() {
+  try {
+    mongoose.connect(DB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      autoIndex: true,
+    });
+    app.listen(PORT, () => {
+      console.log("Server started on port " + PORT);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 start();
 
 /*routes*/
-app.use('/api/client', client);
-app.use('/api/admin', admin);
-app.use('/api/auth', auth);
+app.use("/api/client", client);
+app.use("/api/admin", admin);
+app.use("/api/auth", auth);
---
+++ server/config/allowed_origins.js
@@ -0,0 +1,12 @@
+const allowed_origins = [
+  "http://localhost:3000",
+  "https://complend.shopgeorgia.ge",
+  "http://hypercubic-admin.shopgeorgia.ge/",
+  "http://hypercubic-admin.shopgeorgia.ge",
+  "http://hypercubic.shopgeorgia.ge/",
+  "http://hypercubic.shopgeorgia.ge",
+  "http://hypercubic-api.shopgeorgia.ge/",
+  "http://hypercubic-api.shopgeorgia.ge",
+];
+
+module.exports = allowed_origins;
