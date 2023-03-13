03c4d09c0aa4efc4720acc85f8150de7df50e490
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-03-13 20:41:22 +0400

    images

--- server/config/allowed_origins.js
+++ server/config/allowed_origins.js
@@ -5,12 +5,14 @@ const allowed_origins = [
   "http://localhost:8081",
   "http://localhost:8081/",
   "https://complend.shopgeorgia.ge",
+  "https://hypercubic-admin.shopgeorgia.ge/",
+  "https://hypercubic-admin.shopgeorgia.ge",
   "http://hypercubic-admin.shopgeorgia.ge/",
   "http://hypercubic-admin.shopgeorgia.ge",
-  "http://hypercubic.shopgeorgia.ge/",
-  "http://hypercubic.shopgeorgia.ge",
-  "http://hypercubic-api.shopgeorgia.ge/",
-  "http://hypercubic-api.shopgeorgia.ge",
+  "https://hypercubic.shopgeorgia.ge/",
+  "https://hypercubic.shopgeorgia.ge",
+  "https://hypercubic-api.shopgeorgia.ge/",
+  "https://hypercubic-api.shopgeorgia.ge",
 ];
 
 module.exports = allowed_origins;
