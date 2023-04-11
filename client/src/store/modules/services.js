3c8b5e59c8c0b528e494eb4f860b96be2d77752e
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-04-11 18:33:41 +0400

    created services module in client side

---
+++ client/src/store/modules/services.js
@@ -0,0 +1,31 @@
+import mainMixin from "@/mixins/main";
+import axios from "axios";
+import {SERVER_VARIABLES} from '@/variables'
+
+export default {
+    state:{ 
+        services: null
+    }, 
+    getters:{
+        getServices(state){
+            console.log(state, 'state')
+            return mainMixin.methods.translate(state.services);
+        }
+    },
+    mutations:{
+        updateServices(state, data){
+            state.services = data;
+        },
+    },
+    actions:{
+        async getServicesApi(context){ 
+            const res = await axios({
+                method: 'get',
+                baseURL: SERVER_VARIABLES.SERVER_API + 'services',
+            });
+            if(res.status === 200){
+                context.commit('updateServices', res.data.results);
+            }
+        },
+    }
+};
